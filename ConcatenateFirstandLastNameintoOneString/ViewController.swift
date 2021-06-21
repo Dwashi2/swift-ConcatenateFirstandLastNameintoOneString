8b76ba8e197cdad5c2aaa28634feae1cdf5b8c2b
Daniel Washington <danielvertigo@hotmail.com>
2021-06-21 10:35:34 -0300

    Initial Commit

---
+++ ConcatenateFirstandLastNameintoOneString/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConcatenateFirstandLastNameintoOneString
+//
+//  Created by Daniel Washington Ignacio on 21/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
